e23438ae91d44890134847d2348e33e8560d5023
thisisparth187 <140729488+thisisparth187@users.noreply.github.com>
2025-01-27 21:19:55 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -5,7 +5,7 @@ list_choices = ['rock', 'paper', 'scissor']
 def start():
     u_ch = user_choice()
     b_ch = bot_choice()
-    winner = win_check(u_ch,b_ch)
+    winner = win_check(b_ch,u_ch)
     if winner == 'Tie':
         print("It's a tie")
     else:
@@ -22,9 +22,19 @@ def bot_choice():
 def win_check(b_ch, u_ch):
     if b_ch == u_ch:
         return 'Tie'
+    elif list_choices.index(u_ch) == -1:
+        print("Wrong input value!!")
+        exit()
     elif (b_ch == 'rock' and u_ch == 'scissor') or (b_ch == 'paper' and u_ch == 'rock') or (b_ch == 'scissor' and u_ch == 'paper'):
         return  'Bot'
     else:
         return 'User'
 
-start()
+def game_loop():
+    while True:
+        start()
+        ch = input('Want to play again(Yes/No)?: ')
+        if (ch == 'No') or (ch == 'no'):
+            break
+
+game_loop()
